0ddc9e6e53064d4c4d0c45d5b0178bb97e167f12
SohrabYavari <sohrabyavari00@gmail.com>
2025-04-11 15:16:13 +0100

    listen file update

--- src/server/listen.ts
+++ src/server/listen.ts
@@ -1,14 +1,9 @@
 import server from './server'
 
-const start = async () => {
-    try {
-      await server.listen({port: 3000, host: "0.0.0.0"});
-      console.log("Server is listening on http://localhost:3000");
-    } catch (err) {
-      server.log.error(err);
-      process.exit(1);
-    }
-  };
-  
-  start();
-  
+server.listen({ port: Number(process.env.PORT), host: '0.0.0.0' }, (err, address) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+  console.log(`Server listening on ${address}`);
+});
