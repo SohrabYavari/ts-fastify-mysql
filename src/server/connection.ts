c201886609a01f0e7fd7f300524e13f185e685b8
SohrabYavari <sohrabyavari00@gmail.com>
2025-04-11 15:04:39 +0100

    heroku url added

--- src/server/connection.ts
+++ src/server/connection.ts
@@ -8,6 +8,7 @@ const db = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
+  uri: process.env.JAWSDB_URL,
 });
 
 export default db
