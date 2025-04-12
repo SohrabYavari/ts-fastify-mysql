45ed6c8a0cd7e5a7e0465beb60f735f78261c763
SohrabYavari <sohrabyavari00@gmail.com>
2025-04-12 17:04:39 +0100

    connection file changes

--- src/server/connection.ts
+++ src/server/connection.ts
@@ -1,6 +1,7 @@
 import mysql from "mysql2/promise";
 import dotenv from "dotenv";
 import path from "path";
+import { URL } from "url";
 
 const ENV = process.env.NODE_ENV || "dev";
 const envPath = path.resolve(__dirname, `../../.env.${ENV}`);
@@ -8,9 +9,10 @@ dotenv.config({ path: envPath });
 
 let config: any = {};
 
-if (ENV === "prod") {
+if (ENV === "prod" && process.env.JAWSDB_URL) {
+  const dbUrl = new URL(process.env.JAWSDB_URL);
   config = {
-    connectionString: process.env.JAWSDB_URL,
+    database: dbUrl.pathname.replace("/", ""),
     // host: process.env.DB_HOST,
     // user: process.env.DB_USER,
     // password: process.env.DB_PASSWORD,
